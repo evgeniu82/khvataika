d866c9c534ed503dc4d5ca0fe9d6772fcd0cb4ab
evgeniu82 <evgeniu.tsepaev19@gmail.com>
2026-09-09 23:24:45 +0300

    Fix formatting in KhvataikaAlarmReceiver.java

--- addons/khvataika_background_notifications/KhvataikaAlarmReceiver.java
+++ addons/khvataika_background_notifications/KhvataikaAlarmReceiver.java
@@ -1091,4 +1091,5 @@ public class KhvataikaAlarmReceiver extends BroadcastReceiver {
                 builder.build()
         );
         }
-                                                                     }
+}
+                                                                     
